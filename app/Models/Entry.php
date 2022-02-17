29e511e2fb4e809f7d89c69fbd9e617c92c697ac
Azhar <33951589+qctfw@users.noreply.github.com>
2022-02-17 08:49:04 +0700

    Remove Soft Deletes from Entry

--- app/Models/Entry.php
+++ app/Models/Entry.php
@@ -4,11 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Entry extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     /**
      * The table associated with the model.
