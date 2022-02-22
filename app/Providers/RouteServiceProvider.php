7ac1ef185ca19b1f21e7891c93446b62ae31b087
Azhar <33951589+qctfw@users.noreply.github.com>
2022-02-22 09:45:39 +0700

    Reconfigure Route Service and Configurations

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -20,15 +20,6 @@ class RouteServiceProvider extends ServiceProvider
     public const HOME = '/';
 
     /**
-     * The controller namespace for the application.
-     *
-     * When present, controller route declarations will automatically be prefixed with this namespace.
-     *
-     * @var string|null
-     */
-    // protected $namespace = 'App\\Http\\Controllers';
-
-    /**
      * Define your route model bindings, pattern filters, etc.
      *
      * @return void
@@ -38,10 +29,11 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::prefix('v1')
-                ->middleware('api')
+            Route::middleware('api')->group(function () {
+                Route::prefix('v1')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.v1.php'));
+            });
         });
     }
 
